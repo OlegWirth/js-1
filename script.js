dee90096be10720d3ec4c74723795f274d4d2ce9
OlegW <Wirtholeg@gmail.com>
2022-07-07 17:15:31 +0300

    js_1

---
+++ script.js
@@ -0,0 +1,121 @@
+ 
+ 
+ // 2-------------------------
+
+ let a1 = 5 % 3,
+ a2 = 3 % 5,
+ a3 = 5 + '3',
+ a4 = '5' - 3,
+ a5 = 75 + 'кг',
+ a6 = 785 * 653,
+ a7 = 100 / 25,
+ a8 = 0 * 0,
+ a9 = 0 / 2,
+ a10 = 89 / 0,
+ a11 = 98 + 2,
+ a12 = 5 - 98,
+ a13 = (8 + 56 * 4) / 5,
+ a14 = (9 - 12) * 7 / (5 + 2),
+ a15 = +'123',
+ a16 = 1 || 0,
+ a17 = false || true,
+ a18 = true > 0;
+
+console.log(a1, typeof a1)
+console.log(a2, typeof a2)
+console.log(a3, typeof a3)
+console.log(a4, typeof a4)
+console.log(a5, typeof a5)
+console.log(a6, typeof a6)
+console.log(a7, typeof a7)
+console.log(a8, typeof a8)
+console.log(a9, typeof a9)
+console.log(a10, typeof a10)
+console.log(a11, typeof a11)
+console.log(a12, typeof a12)
+console.log(a13, typeof a13)
+console.log(a14, typeof a14)
+console.log(a15, typeof a15)
+console.log(a16, typeof a16)
+console.log(a17, typeof a17)
+console.log(a18, typeof a18)
+
+// 3-------------------------
+
+let width = 23,
+ height = 10;
+let SPryam = width * height;
+console.log(SPryam + ' см.');
+
+// 4----------V = p * r2 * h---------------
+
+let h = 10,
+ r = 2,
+ p = 3.14;
+let VCilindra = p * r * r * h;
+console.log(VCilindra + ' ltr.');
+
+//5----------------------------------------
+
+let r1 = 5,
+ p1 = 3.14;
+let SKruga = p1 * r1 * r1;
+console.log(SKruga + ' см.');
+
+//6-----s=0.5(a + b)* h--------------------
+
+let a = 5,
+    b = 7,
+    h1 = 10,
+    STrap = 0.5 * (a + b) * h;
+console.log(STrap + ' см.'); 
+
+//7----------------------------------
+
+let S = 2000000,
+    PGS = 10,
+    Y = 5,
+    Pereplata = (S * 0.1) * 5;
+console.log('Pereplata  ' + Pereplata + ' rub.'); 
+
+//8----a+2(x-b)=16-----------b(x+15)=a+6x ----------x+2x+ax+bx=23780.----
+            let aa = 8,
+                bb = 3,
+                xx1 = (16 + 2 * bb - aa)/ 2,
+                xx2 = (15 * bb - aa) / (6 - bb),
+                xx3 = 23780 / (3 + aa + bb);
+        console.log('xx1  = '+ xx1)
+        console.log('xx2  = '+ xx2)
+        console.log('xx3  = '+ xx3)
+
+    
+
+   
+
+
+
+                 
+    
+
+
+                             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
